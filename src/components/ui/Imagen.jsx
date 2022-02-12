bc2f30e486020c661cf3984e4c90278bb5cdb487
Denisa Ponce <denisaponce2012@hotmail.com>
2022-02-11 22:21:32 -0300

    Iniciado

---
+++ src/components/ui/Imagen.jsx
@@ -0,0 +1,16 @@
+// import './imagen.css';
+
+// const Imagen = ((img, type = 'normal', alt));
+// return (
+//   <>
+//     {type === 'logo' ? (
+//       <div className='text-logo'>
+//         <img
+//           src={img}
+//           alt={alt}
+//           className={type === 'type' ? 'logo' : 'normal'} />
+//       </div>
+//     );}
+//   </>
+// )
+
